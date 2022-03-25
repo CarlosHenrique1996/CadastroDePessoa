4d1d5da522a0f1817af8447dec4df5c25a40c21c
CarlosCarlosHenrique1996 <carlossouzarodrigues@live.com>
2022-03-25 17:28:49 -0300

    validacoes HTML

--- templates/js/scriptCadastrar.js
+++ templates/js/scriptCadastrar.js
@@ -1,20 +1,76 @@
 class Cadastro {
 
     constructor(){
+        this.arrayDados = []
 
     }
 
     salva(){         
-        document.getElementById('idNome').value = "";
-        document.getElementById('idCPF').value = "";
-        document.getElementById('idTelefone').value = "";
-        document.getElementById('idEmail').value = "";
-        document.getElementById('idCEP').value = "";
-        document.getElementById('idEndereco').value = "";
-        document.getElementById('idBairro').value = "";
-        document.getElementById('idCidade').value = "";
-        document.getElementById('idEstado').value = "";
-        alert('Enviado com sucesso!')      
+        let dados = this.lerDados();
+
+        if(this.validaCampos(dados) == true){
+            alert('teste') 
+        }
+        
+        console.log(dados);
+    }
+    lerDados(){
+        let dados = {}
+
+        dados.nome = document.getElementById('idNome').value;
+        dados.cpf = document.getElementById('idCPF').value;
+        dados.telefone = document.getElementById('idTelefone').value;
+        dados.email = document.getElementById('idEmail').value;
+        dados.cep = document.getElementById('idCEP').value;
+        dados.endereco = document.getElementById('idEndereco').value;
+        dados.numero = document.getElementById('idNumero').value;
+        dados.bairro = document.getElementById('idBairro').value;
+        dados.cidade = document.getElementById('idCidade').value;
+        dados.estado = document.getElementById('idEstado').value;
+
+        return dados;
+    }
+
+    validaCampos(dados){
+        let msg = '';
+
+        if(dados.nome == ''){
+            msg += '- Campo NOME vazio\n';
+        }
+        if(dados.cpf == ''){
+            msg += '- Campo CPF vazio\n';
+        }
+        if(dados.telefone == ''){
+            msg += '- Campo TELEFONE vazio\n';
+        }
+        if(dados.email == ''){
+            msg += '- Campo E-MAIL vazio\n';
+        }
+        if(dados.cep == ''){
+            msg += '- Campo CEP vazio\n';
+        }
+        if(dados.endereco == ''){
+            msg += '- Campo ENDEREÇO vazio\n';
+        }
+        if(dados.numero == ''){
+            msg += '- Campo NUMERO vazio\n';
+        }
+        if(dados.bairro == ''){
+            msg += '- Campo BAIRRO vazio\n';
+        }
+        if(dados.cidade == ''){
+            msg += '- Campo CIDADE vazio\n';
+        }
+        if(dados.estado == ''){
+            msg += '- Campo ESTADO vazio\n';
+        }
+        if(msg != ''){
+            alert(msg);
+            return false
+        }
+
+        return true;
+
     }
 
     limpa(){         
