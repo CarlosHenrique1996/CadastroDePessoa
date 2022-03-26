bd1b09b3d646cc0b544372abf554b641a8b7b81b
CarlosCarlosHenrique1996 <carlossouzarodrigues@live.com>
2022-03-25 22:42:16 -0300

    Finalizado parte de GET na API

--- templates/js/scriptListar.js
+++ templates/js/scriptListar.js
@@ -1,7 +1,28 @@
 class Lista {
-
     constructor(){
     }
+
+    fazGet(url) {
+        let request = new XMLHttpRequest()
+        request.open("GET", url, false)
+        request.send()
+        return request.responseText
+    }
+
+    dados(){
+        let data = this.fazGet("http://localhost:3000/api/cadastrados");
+        let usuarios = JSON.parse(data);
+        return usuarios
+    }
+
+    listaTabela(){
+        const tbody = document.getElementById('tbody')
+        const dados = this.dados()
+        console.log(dados)
+    }
+
+
+
 }
 
 var lista = new Lista();
